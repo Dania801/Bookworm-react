b4a9a55fb66ed371b217a133fc1d66ada61ef850
Dania801 <ssansnnom07@gmail.com>
2018-07-24 11:36:43 +0300

    Make async request to server and render server validation errors

---
+++ src/actions/auth.js
@@ -0,0 +1,11 @@
+import { USER_LOGGED_IN } from '../types';
+import api from '../api';
+
+export const userLoggedIn = (user) => ({
+    type: USER_LOGGED_IN,
+    user
+})
+
+// make an api request, then take the result and dispatch an action
+export const login = (credentials) => (dispatch) => 
+    api.user.login(credentials).then(user => dispatch(userLoggedIn(user)));
