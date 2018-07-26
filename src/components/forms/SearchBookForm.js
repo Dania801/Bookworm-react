a97026a4a5d6a20e363ca831d2a045d488352a48
Dania801 <ssansnnom07@gmail.com>
2018-07-26 10:47:52 +0300

    Populating books

--- src/components/forms/SearchBookForm.js
+++ src/components/forms/SearchBookForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Form, Dropdown } from 'semantic-ui-react';
 import axios from 'axios';
+import PropTypes from 'prop-types';
 
 class SearchBookForm extends React.Component{
     state = {
@@ -34,6 +35,24 @@ class SearchBookForm extends React.Component{
         this.setState({ loading: true });
         axios(`/api/books/search?q=${this.state.query}`)
             .then(res => res.data.books)
+            .then(books => {
+                const options = [];
+                const booksHash = {};
+                books.forEach(book => {
+                    booksHash[book.goodreadsId] = book;
+                    options.push({
+                        key: book.goodreadsId, 
+                        value: book.goodreadsId, 
+                        text: book.title
+                    })
+                });
+                this.setState({ loading: false, options, books: booksHash})
+            })
+    }
+
+    onChange = (e, data) => {
+        this.setState({ query: data.value });
+        this.props.onBookSelect(this.state.books[data.value]);
     }
 
     render(){
@@ -47,10 +66,15 @@ class SearchBookForm extends React.Component{
                     onSearchChange = {this.onSearchChange}
                     options={this.state.options}
                     loading={this.state.loading}
+                    onChange={this.onChange}
                 />
             </Form>
         )
     }
+}
+
+SearchBookForm.propTypes = {
+    onBookSelect: PropTypes.func.isRequired
 }
 
 export default SearchBookForm;
